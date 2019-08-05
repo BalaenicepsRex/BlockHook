6ee9921d369cf337864f6d6b49cf5caa5c7357f1
杨萧玉 <yulingtianxia@gmail.com>
2019-08-05 23:58:12 +0800

    test

--- BlockHook/BlockHook.m
+++ BlockHook/BlockHook.m
@@ -232,8 +232,19 @@ static void BHFFIClosureFunc(ffi_cif *cif, void *ret, void **args, void *userdat
 
 @end
 
+struct BHInvocatonStruct {
+    void *isa;
+    void *frame;
+    void *retdata;
+    void *signature;
+    void *container;
+    uint8_t retainedArgs;
+    uint8_t reserved[15];
+};
+
 @interface BHInvocation ()
 
+@property (nonatomic) NSInvocation *invocation;
 @property (nonatomic, readwrite, weak) BHToken *token;
 @property (nonatomic, readwrite) void *_Nullable *_Null_unspecified args;
 @property (nonatomic, nullable, readwrite) void *retValue;
@@ -272,25 +283,15 @@ static void BHFFIClosureFunc(ffi_cif *cif, void *ret, void **args, void *userdat
 
 - (BOOL)isArgumentsRetained
 {
-    __block BOOL temp;
-    dispatch_sync(self.argumentsRetainedQueue, ^{
-        temp = self->_argumentsRetained;
-    });
-    return temp;
-}
-
-- (void)setArgumentsRetained:(BOOL)argumentsRetained
-{
-    dispatch_barrier_async(self.argumentsRetainedQueue, ^{
-        self->_argumentsRetained = argumentsRetained;
-    });
+    return self.invocation.argumentsRetained;
 }
 
 #pragma mark - Public Method
 
 - (void)invokeOriginalBlock
 {
-    [self.token invokeOriginalBlockWithArgs:self.realArgs retValue:self.realRetValue];
+    struct BHInvocatonStruct *inv = (__bridge struct BHInvocatonStruct *)(self.invocation);
+    [self.token invokeOriginalBlockWithArgs:inv->frame retValue:inv->retdata];
 }
 
 - (NSMethodSignature *)methodSignature
@@ -300,128 +301,27 @@ static void BHFFIClosureFunc(ffi_cif *cif, void *ret, void **args, void *userdat
 
 - (void)retainArguments
 {
-    if (!self.isArgumentsRetained) {
-        self.dataArgs = [NSMutableData dataWithLength:self.numberOfRealArgs * sizeof(void *)];
-        self.retainMap = [NSMutableDictionary dictionaryWithCapacity:self.numberOfRealArgs + 1];
-        self.mallocMap = [NSMutableDictionary dictionaryWithCapacity:self.numberOfRealArgs + 1];
-        void **args = self.dataArgs.mutableBytes;
-        for (NSUInteger idx = 0; idx < self.numberOfRealArgs; idx++) {
-            const char *type = NULL;
-            if (self.token.hasStret) {
-                if (idx == 0) {
-                    type = self.methodSignature.methodReturnType;
-                }
-                else {
-                    type = [self.methodSignature getArgumentTypeAtIndex:idx - 1];
-                }
-            }
-            else {
-                type = [self.methodSignature getArgumentTypeAtIndex:idx];
-            }
-            args[idx] = [self _copyPointer:self.realArgs[idx] encode:type key:@(idx)];
-            [self _retainPointer:args[idx] encode:type key:@(idx)];
-        }
-        self.realArgs = args;
-        if (self.token.hasStret) {
-            self.args = args + 1;
-            self.retValue = *((void **)args[0]);
-        }
-        else {
-            void *ret = [self _copyPointer:self.retValue encode:self.methodSignature.methodReturnType key:@-1];
-            [self _retainPointer:ret encode:self.methodSignature.methodReturnType key:@-1];
-            self.args = args;
-            self.retValue = ret;
-            self.realRetValue = ret;
-        }
-        
-        self.argumentsRetained = YES;
-    }
+    [self.invocation retainArguments];
 }
 
 - (void)getReturnValue:(void *)retLoc
 {
-    if (!retLoc || !self.retValue) {
-        return;
-    }
-    NSUInteger retSize = self.methodSignature.methodReturnLength;
-    memcpy(retLoc, self.retValue, retSize);
+    [self.invocation getReturnValue:retLoc];
 }
 
 - (void)setReturnValue:(void *)retLoc
 {
-    if (!retLoc || !self.retValue) {
-        return;
-    }
-    NSUInteger retSize = self.methodSignature.methodReturnLength;
-    if (self.isArgumentsRetained) {
-        [self _retainPointer:retLoc encode:self.methodSignature.methodReturnType key:@-1];
-    }
-    memcpy(self.retValue, retLoc, retSize);
+    [self.invocation setReturnValue:retLoc];
 }
 
 - (void)getArgument:(void *)argumentLocation atIndex:(NSInteger)idx
 {
-    if (!argumentLocation || !self.args || !self.args[idx]) {
-        return;
-    }
-    void *arg = self.args[idx];
-    const char *type = [self.methodSignature getArgumentTypeAtIndex:idx];
-    NSUInteger argSize;
-    NSGetSizeAndAlignment(type, &argSize, NULL);
-    memcpy(argumentLocation, arg, argSize);
+    [self.invocation getArgument:argumentLocation atIndex:idx];
 }
 
 - (void)setArgument:(void *)argumentLocation atIndex:(NSInteger)idx
 {
-    if (!argumentLocation || !self.args || !self.args[idx]) {
-        return;
-    }
-    void *arg = self.args[idx];
-    const char *type = [self.methodSignature getArgumentTypeAtIndex:idx];
-    NSUInteger argSize;
-    NSGetSizeAndAlignment(type, &argSize, NULL);
-    if (self.isArgumentsRetained) {
-        [self _retainPointer:argumentLocation encode:type key:@(idx)];
-    }
-    memcpy(arg, argumentLocation, argSize);
-}
-
-#pragma mark - Private Helper
-
-- (void *)_copyPointer:(void **)pointer encode:(const char *)encode key:(NSNumber *)key
-{
-    NSUInteger pointerSize;
-    NSGetSizeAndAlignment(encode, &pointerSize, NULL);
-    NSMutableData *pointerData = [NSMutableData dataWithLength:pointerSize];
-    self.mallocMap[key] = pointerData;
-    void *pointerBuf = pointerData.mutableBytes;
-    memcpy(pointerBuf, pointer, pointerSize);
-    return pointerBuf;
-}
-
-- (void)_retainPointer:(void **)pointer encode:(const char *)encode key:(NSNumber *)key
-{
-    void *p = *pointer;
-    if (!p) {
-        return;
-    }
-    if (encode[0] == '@') {
-        id arg = (__bridge id)p;
-        if (strcmp(encode, "@?") == 0) {
-            self.retainMap[key] = [arg copy];
-        }
-        else {
-            self.retainMap[key] = arg;
-        }
-    }
-    else if (encode[0] == '*') {
-        char *arg = p;
-        NSMutableData *data = [NSMutableData dataWithLength:sizeof(char) * strlen(arg)];
-        self.retainMap[key] = data;
-        char *str = data.mutableBytes;
-        strcpy(str, arg);
-        *pointer = str;
-    }
+    [self.invocation setArgument:argumentLocation atIndex:idx];
 }
 
 @end
@@ -921,10 +821,10 @@ static void BHFFIClosureFunc(ffi_cif *cif, void *ret, void **args, void *userdat
     }
     *(void **)userRet = NULL;
     BHInvocation *invocation = [[BHInvocation alloc] initWithToken:token];
-    invocation.args = userArgs;
-    invocation.retValue = userRet;
-    invocation.realArgs = args;
-    invocation.realRetValue = ret;
+    invocation.invocation = [NSInvocation invocationWithMethodSignature:token.originalBlockSignature];
+    struct BHInvocatonStruct *inv = (__bridge struct BHInvocatonStruct *)(invocation.invocation);
+    inv->frame = userArgs;
+    inv->retdata = userRet;
     if (BlockHookModeContainsMode(token.mode, BlockHookModeBefore)) {
         [token invokeAspectBlockWithArgs:userArgs retValue:userRet mode:BlockHookModeBefore invocation:invocation];
     }
